2534de913bbdd029ce335f8636a2fbe1f9ef0ace
Luca Bruno <lucab@debian.org>
2013-04-25 09:45:02 +0200

    Synch all register to restore initial state
    Signed-off-by: Luca Bruno <lucab@debian.org>

--- qemu/hw/configurable_machine.c
+++ qemu/hw/configurable_machine.c
@@ -30,6 +30,10 @@
 #include "qdict.h"
 #include "exec-memory.h"
 
+#ifdef TARGET_ARM
+#include <target-arm/helper.h>
+#endif
+
 /* Board init.  */
 
 /* The AB and PB boards both use the same core, just with different
@@ -45,56 +49,56 @@ static QDict * load_configuration(const char * filename)
     char * filedata = NULL;
     ssize_t err;
     QObject * obj;
-    
+
     lseek(file, 0, SEEK_SET);
-    
+
     filedata = g_malloc(filesize);
-    
+
     if (!filedata)
     {
         fprintf(stderr, "Out of memory\n");
         exit(1);
     }
-    
+
     err = read(file, filedata, filesize);
-    
+
     if (err != filesize)
     {
         fprintf(stderr, "Reading configuration file failed\n");
         exit(1);
     }
-    
+
     close(file);
-    
+
     obj = qobject_from_jsonv(filedata, NULL);
-    
+
     if (!obj || qobject_type(obj) != QTYPE_QDICT)
     {
         fprintf(stderr, "Error parsing JSON configuration file\n");
         exit(1);
     }
-    
+
     g_free(filedata);
-    
+
     return qobject_to_qdict(obj);
 }
 
-/** 
+/**
  * Return how many characters of the string are part of the directory path, or 0 if the file is specified without directory.
  */
 static int get_dirname_len(const char * filename)
 {
     int i;
-    
+
     for (i = strlen(filename) - 1; i >= 0; i--)
     {
         //FIXME: This is only Linux-compatible ...
         if (filename[i] == '/')
         {
             return i + 1;
-        }   
+        }
     }
-    
+
     return 0;
 }
 
@@ -129,35 +133,75 @@ static void machine_init_state(QDict * conf, CPUArchState * cpu, uint64_t * pc) 
 		for (elm = (QDictEntry *) qdict_first(reg); elm != NULL; elm = (QDictEntry *) qdict_next(reg, elm)) {
 			const char * regname;
 			g_assert(qobject_type(qdict_entry_value(elm)) == QTYPE_QINT);
-			regname = qdict_entry_key(elm);
+			regname = strdup(qdict_entry_key(elm));
 			regvalue = qint_get_int(qobject_to_qint(qdict_entry_value(elm)));
 
 #ifdef TARGET_ARM
 			// TODO: this is very fragile, rework it in a sort-of key filtering
-			if (regname[0] == 'r') {
-				WR_cpu(cpu, regs[atoi(&regname[1])], regvalue);
-				continue;
-			}
-			if (strcmp(regname, "sp") == 0) {
-				WR_cpu(cpu, regs[13], regvalue);
-				continue;
-			}
-			if (strcmp(regname, "lr") == 0) {
-				WR_cpu(cpu, regs[14], regvalue);
-				continue;
-			}
-			if (strcmp(regname, "pc") == 0) {
-				*pc = regvalue;
-				continue;
-			}
-			if (strcmp(regname, "cpsr") == 0) {
-				cpsr_write(cpu, regvalue, 0xFFFFFFFF);
-				continue;
+			char * separator = strchr(regname, '_');
+			if (separator == NULL) {
+				//base registers
+				if (regname[0] == 'r') {
+					WR_cpu(cpu, regs[atoi(&regname[1])], regvalue);
+				}
+				else if (strcmp(regname, "sp") == 0) {
+					WR_cpu(cpu, regs[13], regvalue);
+				}
+				else if (strcmp(regname, "lr") == 0) {
+					WR_cpu(cpu, regs[14], regvalue);
+				}
+				else if (strcmp(regname, "pc") == 0) {
+					*pc = regvalue;
+				}
+				else if (strcmp(regname, "cpsr") == 0) {
+					cpsr_write(cpu, regvalue, 0xFFFFFFFF);
+				}
+			} else {
+				// banked registers
+				int mode=0;
+				*separator = '\0';
+				if (strcmp(separator+1, "fiq") == 0) {
+					mode = ARM_CPU_MODE_FIQ;
+					if (regname[0] == 'r') {
+						WR_cpu(cpu, fiq_regs[atoi(&regname[1])-8], regvalue);
+					}
+				}
+				else if (strcmp(separator+1, "irq") == 0) {
+					mode = ARM_CPU_MODE_IRQ;
+				}
+				else if (strcmp(separator+1, "und") == 0) {
+					mode = ARM_CPU_MODE_UND;
+				}
+				else if (strcmp(separator+1, "usr") == 0) {
+					mode = ARM_CPU_MODE_USR;
+				}
+				else if (strcmp(separator+1, "sys") == 0) {
+					mode = ARM_CPU_MODE_SYS;
+				}
+				else if (strcmp(separator+1, "svc") == 0) {
+					mode = ARM_CPU_MODE_SVC;
+				}
+				else if (strcmp(separator+1, "abt") == 0) {
+					mode = ARM_CPU_MODE_ABT;
+				}
+
+				if (mode != 0) {
+					if (strcmp(regname, "sp") == 0) {
+						helper_set_r13_banked(cpu, mode, regvalue);
+					}
+					else if (strcmp(regname, "lr") == 0) {
+						helper_set_r14_banked(cpu, mode, regvalue);
+					}
+					else if (strcmp(regname, "spsr") == 0) {
+						helper_set_spsr_banked(cpu, mode, regvalue);
+					}
+
+				}
 			}
 #elif TARGET_I386
 			g_assert(false && "Not implemented for Intel");
 #endif
-
+			free((void*) regname);
 		}
 	}
 }
@@ -171,7 +215,7 @@ static void board_init(ram_addr_t ram_size,
     CPUArchState * cpu;
     QDict * conf = NULL;
     uint64_t entry_address;
-    
+
     //Load configuration file
     if (kernel_filename)
     {
@@ -181,32 +225,32 @@ static void board_init(ram_addr_t ram_size,
     {
         conf = qdict_new();
     }
-    
+
     //Configure CPU
     if (qdict_haskey(conf, "cpu_model"))
     {
         cpu_model = qdict_get_str(conf, "cpu_model");
         g_assert(cpu_model);
     }
-    
+
 #ifdef TARGET_ARM
     if (!cpu_model) cpu_model = "arm926";
 #endif
-    
+
     printf("Configurable: Adding processor %s\n", cpu_model);
-    
+
     cpu = cpu_init(cpu_model);
-    
+
     if (!cpu)
     {
         fprintf(stderr, "Unable to find CPU definition\n");
         exit(1);
     }
-    
+
 #ifdef CONFIG_S2E
     s2e_register_cpu(g_s2e, g_s2e_state, cpu);
 #endif
-    
+
     //Configure memory
     if (qdict_haskey(conf, "memory_map"))
     {
@@ -214,7 +258,7 @@ static void board_init(ram_addr_t ram_size,
         QList * memory_map = qobject_to_qlist(qdict_get(conf, "memory_map"));
         g_assert(memory_map);
         MemoryRegion *sysmem = get_system_memory();
-        
+
         QLIST_FOREACH_ENTRY(memory_map, entry)
         {
             QDict * mapping;
@@ -230,42 +274,42 @@ static void board_init(ram_addr_t ram_size,
             int is_first_mapping = TRUE;
             int alias_num = 0;
  //           void * ram_ptr;
-            
+
             g_assert(qobject_type(entry->value) == QTYPE_QDICT);
             mapping = qobject_to_qdict(entry->value);
-            
+
             g_assert(qdict_haskey(mapping, "map") && qobject_type(qdict_get(mapping, "map")) == QTYPE_QLIST && !qlist_empty(qobject_to_qlist(qdict_get(mapping, "map"))));
             g_assert(qdict_haskey(mapping, "name") && qobject_type(qdict_get(mapping, "name")) == QTYPE_QSTRING);
             g_assert(!qdict_haskey(mapping, "is_rom") || qobject_type(qdict_get(mapping, "is_rom")) == QTYPE_QBOOL);
             g_assert(qdict_haskey(mapping, "size") && qobject_type(qdict_get(mapping, "size")) == QTYPE_QINT);
             g_assert((qdict_get_int(mapping, "size") & ((1 << 12) - 1)) == 0); //Somewhere deep in QEMU there seems to be a requirement that memory regions have starting addresses with the lowest 12 bit clear
-            
+
             addresses = qobject_to_qlist(qdict_get(mapping, "map"));
             name = qdict_get_str(mapping, "name");
             is_rom = qdict_haskey(mapping, "is_rom") && qdict_get_bool(mapping, "is_rom");
             size = qdict_get_int(mapping, "size");
-            
+
             ram =  g_new(MemoryRegion, 1);
             g_assert(ram);
-            
+
             //TODO: If is ROM, insert ROM here instead of RAM
             memory_region_init_ram(ram, name, size);
-            
+
             QLIST_FOREACH_ENTRY(addresses, address_entry)
             {
                 QDict * address_dict;
                 g_assert(qobject_type(address_entry->value) == QTYPE_QDICT);
                 address_dict = qobject_to_qdict(address_entry->value);
                 g_assert(qdict_haskey(address_dict, "address") && qobject_type(qdict_get(address_dict, "address")) == QTYPE_QINT);
-                
+
                 address = qdict_get_int(address_dict, "address");
-                
+
                 if (is_first_mapping)
                 {
                     printf("Configurable: Adding memory region %s (size: 0x%lx) at address 0x%lx\n", name, size, address);
                     memory_region_add_subregion(sysmem, address, ram);
                     is_first_mapping = FALSE;
-                    
+
 #ifdef CONFIG_S2E
             s2e_register_ram(g_s2e, g_s2e_state,
                   address, size,
@@ -277,15 +321,15 @@ static void board_init(ram_addr_t ram_size,
                     MemoryRegion * ram_alias =  g_new(MemoryRegion, 1);
                     g_assert(ram_alias);
                     char alias_name[60];
-                    
+
                     snprintf(alias_name, sizeof(alias_name), "%s.alias_%d", name, alias_num);
-                    
+
                     printf("Configurable: Adding memory region %s (size: 0x%lx) at address 0x%lx\n", alias_name, size, address);
                     memory_region_init_alias(ram_alias, alias_name, ram, 0, size);
                     memory_region_add_subregion(sysmem, address, ram_alias);
-                    
+
                     /* Adding an alias memory region to S2E via s2e_register_ram causes a segfault, so don't do it. */
-              
+
 //Apparently you only add ram once to S2E - otherwise segfault :/
 // #ifdef CONFIG_S2E
 //             s2e_register_ram(g_s2e, g_s2e_state,
@@ -293,7 +337,7 @@ static void board_init(ram_addr_t ram_size,
 //                   (uint64_t) memory_region_get_ram_ptr(ram_alias), 0, 0, alias_name);
 // #endif
                 }
-                
+
                 alias_num += 1;
             }
 
@@ -303,17 +347,17 @@ static void board_init(ram_addr_t ram_size,
                 const char * filename;
                 int dirname_len = get_dirname_len(kernel_filename);
                 ssize_t err;
-                
+
                 g_assert(qobject_type(qdict_get(mapping, "file")) == QTYPE_QSTRING);
                 filename = qdict_get_str(mapping, "file");
-                
+
                 if (!is_absolute_path(filename))
                 {
                     char * relative_filename = g_malloc0(dirname_len + strlen(filename) + 1);
                     g_assert(relative_filename);
                     strncpy(relative_filename, kernel_filename, dirname_len);
                     strcat(relative_filename, filename);
-                    
+
                     file = open(relative_filename, O_RDONLY | O_BINARY);
                     g_free(relative_filename);
                 }
@@ -321,31 +365,31 @@ static void board_init(ram_addr_t ram_size,
                 {
                     file = open(filename, O_RDONLY | O_BINARY);
                 }
-                
+
                 data_size = lseek(file, 0, SEEK_END);
                 lseek(file, 0, SEEK_SET);
-                
+
                 g_assert(data_size <= size); //Size of data to put into a RAM region needs to fit in the RAM region
-                
+
                 data = g_malloc(data_size);
                 g_assert(data);
-                
+
                 err = read(file, data, data_size);
-                g_assert(err == data_size); 
-                
+                g_assert(err == data_size);
+
                 close(file);
-                
+
                 //And copy the data to the memory, if it is initialized
                 printf("Configurable: Copying 0x%lx byte of data from file %s to address 0x%lx\n", data_size, filename, address);
 //                ram_ptr = qemu_get_ram_ptr(memory_region_get_ram_addr(ram));
 //                memcpy(ram_ptr, data, data_size);
 //                qemu_put_ram_ptr(ram_ptr);
                 cpu_physical_memory_write_rom(address, (uint8_t *) data, data_size);
-            }  
-            
+            }
+
         }
     }
-    
+
     /*
      * The devices stuff is just considered a hack, I want to replace everything here with a device tree parser as soon as I have the time ...
      */
@@ -354,29 +398,29 @@ static void board_init(ram_addr_t ram_size,
         QListEntry * entry;
         QList * devices = qobject_to_qlist(qdict_get(conf, "devices"));
         g_assert(devices);
-        
+
         QLIST_FOREACH_ENTRY(devices, entry)
         {
             QDict * device;
-            
+
             const char * qemu_name;
             const char * bus;
             uint64_t address;
             qemu_irq* irq;
-            
+
             g_assert(qobject_type(entry->value) == QTYPE_QDICT);
             device = qobject_to_qdict(entry->value);
-            
+
             g_assert(qdict_haskey(device, "address") && qobject_type(qdict_get(device, "address")) == QTYPE_QINT);
             g_assert(qdict_haskey(device, "qemu_name") && qobject_type(qdict_get(device, "qemu_name")) == QTYPE_QSTRING);
             g_assert(qdict_haskey(device, "bus") && qobject_type(qdict_get(device, "bus")) == QTYPE_QSTRING);
-            
+
             bus = qdict_get_str(device, "bus");
             qemu_name = qdict_get_str(device, "qemu_name");
             address = qdict_get_int(device, "address");
-            
-            
-            
+
+
+
             if (strcmp(bus, "sysbus") == 0)
             {
                 //For now only dummy interrupts ...
@@ -389,9 +433,9 @@ static void board_init(ram_addr_t ram_size,
             }
         }
     }
-    
+
     //sysbus_create_simple("xilinx,uartlite", UARTLITE_BASEADDR, irq[3]);
-    
+
     //Set PC to entry point
     g_assert(qdict_haskey(conf, "entry_address"));
     g_assert(qobject_type(qdict_get(conf, "entry_address")) == QTYPE_QINT);
